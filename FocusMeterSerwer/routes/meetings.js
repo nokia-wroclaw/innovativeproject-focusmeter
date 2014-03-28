a3698aa84e76ea1e44789b4277a9b3308d484a06
purplecode <niespammnie@gmail.com>
2014-03-28 18:14:51 +0100

    adhoc refactoring session

--- FocusMeterSerwer/routes/meetings.js
+++ FocusMeterSerwer/routes/meetings.js
@@ -1,145 +1,174 @@
-
 /*
  * GET users listing.
  */
 
 
-exports.find = function (db){
-	return function(req, res) {
-		var coll = db.get('meetings');
+exports.find = function(db) {
+    return function(req, res) {
+        var coll = db.get('meetings');
 
-		coll.find({}, function(e,docs) {
-			if(e) {
-				res.send("Błąd w dostępie do bazy danych.");
-			}
-			else {
-				res.json(docs);
-			}
-			
-		});
-	};
-	 	
- 	} ,
+        coll.find({}, function(e, docs) {
+            if (e) {
+                res.send("Błąd w dostępie do bazy danych.");
+            } else {
+                res.json(docs);
+            }
+
+        });
+    };
+
+},
 
 /**
 Funkcja zwraca spotkanie o zadanym kodzie spotkania
 */
 exports.fff = function(db) {
-	return function(req, res) {
-		var coll = db.get('meetings');
+    return function(req, res) {
+        var coll = db.get('meetings');
 
-		coll.find({"meetingCode" : req.params.m}, function(e, docs) {
-			res.json(docs);
-		});
-	};
+        coll.find({
+            "meetingCode": req.params.m
+        }, function(e, docs) {
+            res.json(docs);
+        });
+    };
 },
+
+
+var appendIf = function(condition, array, message) {
+    if (condition) {
+        array.push(message);
+    }
+};
+
+var validate = function(meeting) {
+
+    var messages = [];
+    appendIf(meeting.mac.length != 17, messages, "incorrect meeting mac");
+    appendIf(meeting.date~ / huhuhuhu / , messages, "incorrect meeting mac");
+
+    // //check mac XX:XX:XX:XX:XX:XX
+    // if (mac.length != 17) {
+    //     res.json({
+    //         "message": "incorrect meeting mac"
+    //     });
+    //     return;
+    // }
+
+    // //check date value DD/MM/YYYY
+    // if (date.length != 10) {
+    //     res.json({
+    //         "message": "incorrect meeting date"
+    //     });
+    //     return;
+    // }
+
+    // //check startHour xX:XX XX
+    // if (startHour.length < 6 || startHour.length > 8) {
+    //     res.json({
+    //         "message": "incorrect meeting startHour"
+    //     });
+    //     return;
+    // }
+
+    // //check endHour xX:XX XX
+    // if (endHour.length < 6 || endHour.length > 8) {
+    //     res.json({
+    //         "message": "incorrect meeting endHour"
+    //     });
+    //     return;
+    // }
+
+    // //check title (length=40)
+    // if (title.length < 1 || title.length > 40) {
+    //     res.json({
+    //         "message": "incorrect meeting title"
+    //     });
+    //     return;
+    // }
+
+    // //check meeting code XXXX
+    // if (meetingCode.length != 4) {
+    //     res.json({
+    //         "message": "incorrect meeting meetingCode"
+    //     });
+    //     return;
+    // }
+
+    return messages;
+};
+
+// Collection(collectionName)
+var hasMeetingWithCode = function(collection, code, callbackNotExists, callbackExists) {
+    collection.findOne({
+        "meetingCode": code
+    }, function(e, doc) {
+        if (e) {
+            // handle error
+        } else {
+            if (doc) {
+                callbackExists();
+            } else {
+                callbackNotExists();
+            }
+        }
+    });
+}
 
 
 exports.addMeeting = function(db) {
-	return function(req, res) {
-		var mac = req.body.mac;
-		var date = req.body.date;
-		var startHour = req.body.startHour;
-		var endHour = req.body.endHour;
-		var title = req.body.title;
+    return function(req, res) {
 
-		var coll = db.get('meetings');
+        var meeting = {
+            mac: req.body.mac,
+            date: req.body.date,
+            startHour: req.body.startHour,
+            endHour: req.body.endHour,
+            title: req.body.title
+        };
 
-		function newMeetingCode() {
-				
-			// Generowanie nowego 4-znakowego kodu np. 4GH3
-			var str = Math.random().toString(36).substring(2,6).toUpperCase();
+        var validationMessages = validate(meeting);
+        if (!validate(meeting)) {
+            res.send({
+                'errors': validationMessages
+            });
+            return;
+        }
 
-			// oraz sprawdzenie jego unikalności
-			coll.findOne({"meetingCode" : str}, function(e, doc) {
-				if(doc == null) {
-					console.log("haa! takiego jeszcze nie ma");
-				} else {
-					// Zawsze się wykonuje, nawet jak pusta baza
-					// doc jest zawsze != null
-					console.log(doc);
-					console.log("noooooooo! zajęty...");
-				}				
-			});
+        var str = Math.random().toString(36).substring(2, 6).toUpperCase();
+        var collection = db.get('meetings');
 
-			// Nie sprawdzony string
-			return str;
-		}
+        var successCallback = function() {
+            meeting.meetingCode = str;
+            coll.insert(meeting, function(err, docs) {
+                if (err) {
+                    res.send("Blad przy dodawaniu spotkania.");
+                } else {
+                    res.json({
+                        "meetingCode": meetingCode
+                    });
+                }
+            });
+        };
 
-		var meetingCode = newMeetingCode();
 
-		console.log(meetingCode);
-		
+        var failureCallback = function() {
+            str = Math.random().toString(36).substring(2, 6).toUpperCase();
+            hasMeetingWithCode(collection, str, successCallback, failureCallback);
+        };
 
-		//check mac XX:XX:XX:XX:XX:XX
-		if(mac.length != 17)
-		{
-			res.json({"message" : "incorrect meeting mac"});
-			return;
-		}
+        hasMeetingWithCode(collection, str, successCallback, failureCallback);
 
-		//check date value DD/MM/YYYY
-		if(date.length != 10)
-		{
-			res.json({"message" : "incorrect meeting date"});
-			return;
-		}
-
-		//check startHour xX:XX XX
-		if(startHour.length < 6 || startHour.length > 8)
-		{
-			res.json({"message" : "incorrect meeting startHour"});
-			return;
-		}
-
-		//check endHour xX:XX XX
-		if(endHour.length < 6 || endHour.length > 8)
-		{
-			res.json({"message" : "incorrect meeting endHour"});
-			return;
-		}
-
-		//check title (length=40)
-		if(title.length < 1 || title.length > 40)
-		{
-			res.json({"message" : "incorrect meeting title"});
-			return;
-		}
-
-		//check meeting code XXXX
-		if(meetingCode.length != 4)
-		{
-			res.json({"message" : "incorrect meeting meetingCode"});
-			return;
-		}
-
-		coll.insert({
-			"mac" : mac,
-			"date" : date,
-			"startHour" : startHour,
-			"endHour" : endHour,
-			"title" : title,
-			"meetingCode" : meetingCode
-		}, function(err, docs) {
-			if(err) {
-				res.send("Blad przy dodawaniu spotkania.");
-			}
-			else
-			{
-				res.json({"meetingCode": meetingCode});
-			}
-		});
-	}
+    }
 },
 
 exports.deleteAllMeetings = function(db) {
-	return function(req, res) {
-		var collection = db.get('meetings');
+    return function(req, res) {
+        var collection = db.get('meetings');
 
-		collection.drop();
+        collection.drop();
 
-		res.send("Skasowano wszystkie spotkania.");
-	}
-	
+        res.send("Skasowano wszystkie spotkania.");
+    }
+
 }
-
