91b8368635b3ae6f17da03eed73c77ac417ff795
MaciejBaj <macie.baj@gmail.com>
2014-05-24 15:30:02 +0200

    History Client Side
    Meetings are loading after retreiving response from server. Then they
    trigger sending meeting code after clicking

---
+++ FocusMeterClient/LocalFiles/js/history.js
@@ -0,0 +1,58 @@
+// handling view of history (history.html)
+
+function goBackToStartScreen() {
+    window.location = './index.html';
+}
+
+
+$(document).ready(function () {
+
+   
+    var meetings = ["uno", "duo"];//zamiast tego funkcja ktora zwroci meetings dla danego UUID
+
+    var index = 0;
+    $(meetings).each(function () {
+        InsertMeetingIntoHistory(this, index);
+        index++;
+    })
+
+    //after clicking at any text button
+    $('button[id*="nameButtonElement"]').click(function () {
+        alert("meeting code do przeslania na serwer to: " + $(this).attr('data-meetingcode'));
+    });
+
+});
+
+function InsertMeetingIntoHistory(meeting, index) {
+    //dane tylko testowe, decolowo maj¹ przyjœæ w odpowiedzi
+    meetingNames = ["Co w zbozu pisziczy ", "W pustyni i w puszzy"];
+    meetingDates = ["19-01-2013", "01-07-2014"];
+    meetingCodes = ["KUKU7", "BULWA"];
+
+    var historyContainer = $(".HistoryContent");
+
+    var newSpan = document.createElement("span");
+    newSpan.className = "input-prepend meetingElementInHistory";
+    newSpan.id = "historyElement" + index;
+
+    $(historyContainer).append(newSpan);
+
+    var meetingNameButton = document.createElement("button");
+    meetingNameButton.type = "button";
+    meetingNameButton.id = "nameButtonElement" + index;
+    meetingNameButton.className = "btn btn-success buttonNameInHistory";
+    $(meetingNameButton).text(meetingNames[index]);
+    $(meetingNameButton).attr('data-meetingcode', meetingCodes[index]);
+
+    $(newSpan).append(meetingNameButton);
+
+    var meetingDateButton = document.createElement("button");
+    meetingDateButton.type = "button";
+    meetingDateButton.id = "dateButtonElement" + index;
+    meetingDateButton.className = "btn btn-success buttonDateInHistory";
+    $(meetingDateButton).text(meetingDates[index]);
+
+    $(newSpan).append(meetingDateButton);
+
+}
+
